7a368ea24ae5daea2c51a17a6632db97cfca49e7
Will Plumb <zrenicaplumb@gmail.com>
2020-08-10 19:00:00 -0600

    .

---
+++ app.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", () => {
+     const cardArray = [
+          {
+               name: "blank",
+               img: "img/blank.png",
+          },
+          {
+               name: "cheeseburger",
+               img: "img/cheeseburger.png",
+          },
+          {
+               name: "fries",
+               img: "img/fries.png",
+          },
+          {
+               name: "hotdog",
+               img: "img/hotdog.png",
+          },
+          {
+               name: "ice-cream",
+               img: "img/ice-cream.png",
+          },
+          {
+               name: "milkshake",
+               img: "img/milkshake.png",
+          },
+          {
+               name: "pizza",
+               img: "img/pizza.png",
+          },
+          {
+               name: "white",
+               img: "img/white.png",
+          },
+     ];
+
+     function createBoard() {
+          const grid = document.querySelector(".grid");
+          for (i = 0; i < cardArray.length; i++) {
+               let card = document.createElement("img");
+               card.setAttribute("src", "img/blank.png");
+               card.setAttribute("data-id", i);
+               card.addEventListener("click", flipCard);
+               grid.appendChild(card);
+          }
+     }
+
+     //flip the card
+     function flipCard() {
+          debugger;
+          let cardId = this.getAttribute("data-id");
+     }
+
+     // check for match
+
+     createBoard();
+     console.log(cardArray);
+});
